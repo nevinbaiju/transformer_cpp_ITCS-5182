a2c06ac6b3ccc0291e69dfa9cdf379e0854e1452
Nevin <nevinbaiju@gmail.com>
2023-11-06 18:19:45 -0500

    modified activateions to use 1d data storage format

--- activations.cpp
+++ activations.cpp
@@ -21,3 +21,24 @@ float* relu(float input[], int size, bool inplace) {
         return result; // Return the new result array reference
     }
 }
+
+float* relu(float *input, int rows, int cols, bool inplace) {
+    float* result;
+    int size = rows*cols;
+    if(!inplace){
+        result = new float[size];
+    }
+    for (int i = 0; i < size; ++i) {
+        if (inplace) {
+            input[i] = std::max(input[i], 0.0f);
+        } else {
+            result[i] = std::max(input[i], 0.0f);
+        }
+    }
+
+    if (inplace) {
+        return input;  // Return the input array reference
+    } else {
+        return result; // Return the new result array reference
+    }
+}
--- activations.h
+++ activations.h
@@ -1,1 +1,2 @@
 float* relu(float input[], int size, bool inplace = false);
+float* relu(float *input, int rows, int cols, bool inplace = false);
