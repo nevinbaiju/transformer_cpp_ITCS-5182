b0c6b36666bcca5068d03a96de1c6feb27be7d0c
Nevin <nevinbaiju@gmail.com>
2023-11-13 19:35:57 -0500

    Added identity init exception size mismatch

--- exceptions.cpp
+++ exceptions.cpp
@@ -12,3 +12,13 @@ SizeMismatchException::SizeMismatchException(int r1, int c1, int r2, int c2) {
 const char* SizeMismatchException::what() const noexcept {
     return message_.c_str();
 }
+
+InvalidDimensionException::InvalidDimensionException(int rows, int cols) {
+    std::stringstream message;
+    message << "Given size of Rows: " << rows << "Does not match with the given size of columns: " << cols << std::endl;
+    message_ = message.str();
+}
+
+const char* InvalidDimensionException::what() const noexcept {
+    return message_.c_str();
+}
--- exceptions.h
+++ exceptions.h
@@ -11,3 +11,13 @@ public:
 private:
     std::string message_;
 };
+
+class InvalidDimensionException : public std::exception {
+public:
+    InvalidDimensionException(int r1, int c1, int r2, int c2);
+
+    const char* what() const noexcept override;
+
+private:
+    std::string message_;
+};
