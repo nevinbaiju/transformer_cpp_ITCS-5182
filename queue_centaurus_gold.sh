a986e55d32b61b160025a3c7331feb368154e364
Nevin Baiju <nbaiju@gal-i1.uncc.edu>
2023-12-03 17:53:29 -0500

    queing fixes

--- queue_centaurus_gold.sh
+++ queue_centaurus_gold.sh
@@ -7,4 +7,4 @@ rm results/*
 rm plots/*
 
 sbatch --partition=Centaurus --chdir=`pwd` --time=24:00:00 --ntasks=1 --cpus-per-task=1 --job-name=gold --mem=100G ./bench.sh normal
-sbatch --partition=Centaurus --chdir=`pwd` --time=08:00:00 --ntasks=1 --cpus-per-task=32 --job-name=gold --mem=100G ./bench.sh avx
+sbatch --partition=Centaurus --chdir=`pwd` --time=08:00:00 --ntasks=1 --cpus-per-task=16 --job-name=avx --mem=100G ./bench.sh avx
